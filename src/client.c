c5a057113d9a666eebf0e2d3dfced78627fad609
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-05-17 15:36:20 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -12,41 +12,43 @@
 
 #include "minitalk.h"
 
-static void send_bits(int pid, char i)
-{    
-    int bit;
-    
-    bit = 0;
-    while (bit < 8)
-    {
-        if (i & (1 << bit))
-            kill(pid, SIGUSR1);
-        else 
-            kill(pid, SIGUSR2);
-        usleep(150);
-        bit++;
-    }
+static void	send_bits(int pid, char i)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((i & (1 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int	pid;
-    int i;
-    
-    i = 0;
-    if (argc == 3)
-    {
-        pid = ft_atoi(argv[1]);
-        while (1)
-        {
-            send_bits(pid, argv[2][i]);
-            i++;
-        }
-    }
-    else
-    {
-        ft_printf(RED "Error: Wrong format\n" RESET);
-        ft_printf(RED "Try: ./client <PID> <MESSAGE>\n" RESET);
-    }
-    return (EXIT_FAILURE);
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			send_bits(pid, argv[2][i]);
+			i++;
+		}
+		send_bits(pid, '\n');
+	}
+	else
+	{
+		ft_printf(RED "Error: Wrong format\n" RESET);
+		ft_printf(RED "Try: ./client <PID> <MESSAGE>\n" RESET);
+		return (1);
+	}
+	return (0);
 }
